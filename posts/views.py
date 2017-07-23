4644ae6ae88d4e297b80df05eabb97505047dd09
malawadhi1 <malawadhi1@gmail.com>
2017-07-23 20:39:22 +0300

    created a list and detail page that are linked

--- posts/views.py
+++ posts/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from .models import Post
 from django.shortcuts import get_object_or_404
 
-def post(request):
-	obj = get_object_or_404(Post, id=2)
+def post_list(request):
+	obj_list = Post.objects.all()
+	context = {
+		"obj_list": obj_list,
+		
+
+	}
+	return render(request, 'post_list.html', context)
+
+def post_detail(request, post_id):
+	obj = get_object_or_404(Post, id=post_id)
 	context = {
 		"instance": obj,
 		"user": request.user,
 
 	}
-	return render(request, 'whatever.html', context)
+	return render(request, 'post_detail.html', context)
