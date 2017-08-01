bad58670e81e8c4e9158de730284eded9e9c49e0
malawadhi1 <malawadhi1@gmail.com>
2017-08-01 19:19:57 +0300

    login and sign up

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,72 @@
 from django.shortcuts import render, redirect
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, UserSignUp, UserLogin
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from urllib.parse import quote
+from django.http import Http404
+from django.utils import timezone
+
+from django.contrib.auth import authenticate, login, logout
 
 
+def userlogout(request):
+	logout(request)
+	return redirect("posts:list")
+
+
+def usersignup(request):
+	context = {}
+	form = UserSignUp()
+	context['form'] = form
+	if request.method == "POST":
+		form = UserSignUp(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = user.username
+			password = user.password
+			user.set_password(password)
+			user.save()
+			auth_user = authenticate(username=username, password=password)
+			login(request, auth_user)
+
+
+			return redirect("posts:list")
+		messages.warning(request, forms.errors)
+		return redirect("posts:signup")
+	return render(request, 'signup.html', context)
+
+
+def userlogin(request):
+	context = {}
+	form = UserLogin()
+	context['form'] = form
+	if request.method == "POST":
+		form = UserLogin(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+
+			auth_user = authenticate(username=username, password=password)
+			if auth_user is not None: 
+				login(request, auth_user)
+				return redirect("posts:list")
+
+			messages.warning(request, "Wrong username or password combination")
+			return redirect("posts:login")
+		messages.warning(request, form.errors)
+		return redirect("posts:login")
+	return render(request, 'login.html', context)
+
+ 
 def post_list(request):
-	obj_list = Post.objects.all() #.order_by("-timestamp", "-updated")
-
+	today = timezone.now().date()
+	if request.user.is_staff or request.user.is_superuser:
+		obj_list =  Post.objects.all()
+	else: 
+		obj_list = Post.objects.filter(draft=False).filter(publish__lte=today)
 	paginator = Paginator(obj_list, 6) # Show 4 contacts per page
-
 	page = request.GET.get('page')
 	try:
 		objects = paginator.page(page)
@@ -30,8 +85,8 @@ def post_list(request):
 	}
 	return render(request, 'post_list.html', context)
 
-def post_detail(request, post_id):
-	obj = get_object_or_404(Post, id=post_id)
+def post_detail(request, slug):
+	obj = get_object_or_404(Post, slug = slug)
 	context = {
 		"instance": obj,
 		"user": request.user,
@@ -40,9 +95,13 @@ def post_detail(request, post_id):
 	return render(request, 'post_detail.html', context)
 
 def post_create(request):
+	if not (request.user.is_staff or request.user.is_superuser):
+		raise Http404
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
-		form.save()
+		obj = form.save(commit=False)
+		obj.author = request.user
+		obj.save()
 		messages.success(request, "Congratulations on creating a Post")
 		return redirect("posts:list")
 	context = {
@@ -50,13 +109,14 @@ def post_create(request):
 	}
 	return render(request, 'post_create.html', context)
 
-def post_update(request, post_id):
-	post_object = get_object_or_404(Post, id=post_id)
+def post_update(request, slug):
+	if not (request.user.is_staff or request.user.is_superuser):
+		raise Http404
+	post_object = get_object_or_404(Post, slug=slug)
 	form = PostForm(request.POST or None,  request.FILES or None, instance=post_object)
 	if form.is_valid():
 		form.save()
 		messages.success(request, "Your Post Has Been Updated")
-
 		return redirect("posts:list")
 	context = {
 		"form":form,
@@ -64,8 +124,10 @@ def post_update(request, post_id):
 	}
 	return render(request, 'post_update.html', context)
 
-def post_delete(request, post_id):
-	Post.objects.get(id=post_id).delete()
+def post_delete(request, slug):
+	if not (request.user.is_superuser):
+		raise Http404
+	Post.objects.get(slug=slug).delete()
 	messages.warning(request, "Post Has Been Deleted")
 	return redirect("posts:list")
 
